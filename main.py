1202fa8a92ce7cc7fbfd4dc3aba73ddad062f799
AbderrahimRezki <abderrahim4cem@gmail.com>
2024-09-24 18:48:56 +0100

    removed non necessary files

--- main.py
+++
@@ -1,8 +0,0 @@
-from json_parser import parse_json
-
-print(parse_json('''{
-    "Key": 1
-}
-'''))
-
-print("Done")
--- notmain.py
+++
@@ -1,4 +0,0 @@
-from json_parser import parse_json
-
-print(parse_json('{"key": "value"}'))
-print(parse_json.__name__)
